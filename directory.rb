9285c27b8c9e4b70530f6a48501e896c8983b059
Rupert Pople <rupertpople@gmail.com>
2022-03-10 15:44:12 +0000

    Shortened printing method

--- directory.rb
+++ directory.rb
@@ -1,18 +1,21 @@
 student_count = 11
-
-puts ("The students of Villain Academy")
-puts ("-------------")
-puts ("Dr. Hannibal Lecter")
-puts ("Darth Vader")
-puts ("Nurse Ratched")
-puts ("Michael Corleone")
-puts ("Alex DeLarge")
-puts ("The Wicked Witch of the West")
-puts ("Terminator")
-puts ("Freddy Krueger")
-puts ("The Joker")
-puts ("Joffrey Baratheon")
-puts ("Norman Bates")
-print "Overall, we have "
-print student_count
-print " great students"
+# let's put all students into an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+# and then print them
+puts "The students of Villains Academy"
+puts "-------------"
+puts students[0..10]
+# finally, we print the total
+puts "Overall, we have #{students.count} great students"
